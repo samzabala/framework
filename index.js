8b281d8d9a4bcd9e34cd8bc9558f253bc7895e79
Sam Z <samapitongzabala@gmail.com>
2020-12-01 15:52:25 -0800

    dont look it still messy D:

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import './scss/framework.scss';
 // import Util from './src/core/util';
 // import Internal from './src/core/internal';
 
+// @TODO
+//convert components to classes
+//js events. good lord... trhe events
+//gawen mo teh custom
+//sum xtra ctra javascript to condition statement  mo kung gusto mong core lang or light lang or achuchucu par modULAR NGA
+
 //frameWork shit
 const frameWork = {};
 
